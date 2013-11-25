a566d22da04354096f30585d87fc5e3471be96aa
Claus Strasburger <claus@strasburger.de>
2013-11-25 13:42:32 +0100

    refactored cmd into seperate file

--- src/core/core.go
+++ src/core/core.go
@@ -1,20 +1,10 @@
 package core
 
 import (
-	"bytes"
 	"config"
 	"db"
-	"fmt"
 	"log"
-	"os"
-	"strings"
 )
-
-type Command struct {
-	verbs   []string
-	help    string
-	handler func(args []string)
-}
 
 func Start() error {
 	// load config
@@ -27,11 +17,12 @@ func Start() error {
 		return err
 	}
 
+	initCmd()
+
+	//TODO call plugin loads
+
 	// update local files
 	db.Update()
-	commandMap = make(map[string]Command)
-	registerBaseCommands()
-	//TODO call plugin loads
 
 	return nil
 }
@@ -46,86 +37,4 @@ func Shutdown() error {
 	}
 
 	return err
-}
-
-var commandMap map[string]Command
-
-func RegisterCommand(command Command) {
-	for _, verb := range command.verbs {
-		old, already := commandMap[verb]
-		if already {
-			fmt.Println("error registering verb", verb, `for command "`,
-				command.help, `", it already exists with command "`, old.help, `".`)
-			return
-		}
-	}
-	for _, verb := range command.verbs {
-		commandMap[verb] = command
-	}
-}
-
-func UnregisterCommand(command Command) {
-	for _, verb := range command.verbs {
-		delete(commandMap, verb)
-	}
-}
-
-func registerBaseCommands() {
-	quit := Command{
-		[]string{"quit", "q", "close", "exit"},
-		"Shuts down and exits.",
-		func(_ []string) {
-			Shutdown()
-			runCmdLine = false
-			os.Stdin.Close()
-		}}
-	RegisterCommand(quit)
-
-}
-
-var runCmdLine = true
-
-// start a REPL shell.
-func CmdLine() {
-	log.Println("cfmedias", currentVersion)
-
-	readBuffer := make([]byte, 1)
-	var buffer bytes.Buffer
-
-	for runCmdLine {
-		fmt.Print("> ")
-		buffer.Reset()
-		readBuffer[0] = 0
-		for readBuffer[0] != '\n' {
-			n, err := os.Stdin.Read(readBuffer)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			if n != 1 {
-				fmt.Println("read error!,", readBuffer[0])
-				continue
-			}
-			if readBuffer[0] != '\n' && readBuffer[0] != '\r' {
-				buffer.WriteByte(readBuffer[0])
-			}
-		}
-		cmd := buffer.String()
-		// Parse!
-		split := strings.Split(cmd, " ")
-		if len(split) > 0 {
-			command, ok := commandMap[split[0]]
-			if !ok {
-				fmt.Println("Error: no command for", split[0])
-				continue
-			}
-			command.handler(split[1:])
-		}
-	}
-}
-
-func parseQuoted(s string) string {
-	parsed := s[1 : len(s)-1]
-	parsed = strings.Replace(parsed, `\"`, `"`, -1)
-	return parsed
 }
