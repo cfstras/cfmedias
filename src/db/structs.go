b0ba4ae734757e1e2150630e7ae045bdc1ec07c3
Claus Strasburger <claus@strasburger.de>
2013-12-15 22:50:58 +0100

    Outlining rating concept.

--- src/db/structs.go
+++ src/db/structs.go
@@ -2,7 +2,9 @@ package db
 
 import (
 	"errors"
+	"fmt"
 	"github.com/coopernurse/gorp"
+	"math"
 	"time"
 )
 
@@ -11,7 +13,7 @@ const (
 	FolderTable = "folders"
 )
 
-// when inserting an item, Folder has to be a pointer with only Path set.
+// when inserting an item, Folder has to be a pointer with only the Path set.
 type Item struct {
 	Id            uint64 `db:"item_id"`
 	Title         string `db:"title"`
@@ -26,18 +28,37 @@ type Item struct {
 	Folder   *Folder `db:"-"`
 	FolderId uint64  `db:"folder_id"`
 
-	Added          int64   `db:"added_date"`
-	Rating         float32 `db:"rating"` //TODO define fancy math around this
-	PlayCount      uint32  `db:"play_count"`
-	ScrobbledCount uint32  `db:"scrobbled_count"`
-	SkipCount      uint32  `db:"skip_count"`
+	Added int64 `db:"added"`
+
+	// Total play score of a track.
+	// Gets incremented by up to 1 each time a user listens to a track,
+	// depending on how far in he listened to the track.
+	// The formula for the added value is:
+	//  x: amount of track listened to (0..1)
+	//  tl: listened lower threshold
+	//  tu: listened upper threshold
+	//  add(x) = min( 1, max( 0, (x - tl) / (tu - tl) ) )
+	PlayScore float32 `db:"play_score"`
+
+	// Total times a track was played (including skips)
+	// Track Score := (PlayScore / PlayCount)
+	PlayCount uint32 `db:"play_count"`
+
+	// Total times a track was played and scored as positive (x >= tl)
+	ScoredCount uint32 `db:"scored_count"`
+
+	// Number of plays that are registered at the local scrobbler.
+	//
+	// When a track is listened and scored positively, this is incremented
+	// and a scrobble is sent to the service.
+	ScrobbledCount uint32 `db:"scrobbled_count"`
 }
 
 type Folder struct {
 	Id   uint64 `db:"folder_id"`
 	Path string `db:"path"`
 
-	Added         int64  `db:"added_date_folder"`
+	Added         int64  `db:"added"`
 	MusicbrainzId string `db:"musicbrainz_folder_id"`
 }
 
@@ -45,6 +66,35 @@ type ItemPathView struct {
 	Id       uint64
 	Filename string
 	Path     string
+}
+
+func (i *Item) String() string {
+	return fmt.Sprintf("Item[%d]{%s / %s - %s / %d %s, %s]", i.Id, i.Artist,
+		i.AlbumArtist, i.Album, i.TrackNumber, i.Title, i.Genre)
+}
+
+// Returns a rating of the song and an indication of how accurate the score
+// might be.
+// Both values are in the range of 0 to 1.
+// The computation uses a combination of PlayScore, PlayCount and ScrobbledCount.
+func (i *Item) Rating() (rating, accuracy float32) {
+	//TODO
+
+	// accuracy is based on how much data there is on this track.
+	//TODO use an average data count on the whole database instead of hard one
+	acc := (i.PlayScore - 5) // every data below 5 listens is basically useless
+	score := i.PlayScore / float32(i.PlayCount)
+
+	//incomingScrobbles := i.ScrobbledCount - i.ScoredCount
+
+	//TODO average score over ratio from incoming scrobbles to own data
+	// also use the median of all scrobbles to weigh out the score
+
+	return limit(score, 0, 1), limit(acc, 0, 1)
+}
+
+func (i *Item) Skipped() uint32 {
+	return i.ScoredCount - i.PlayCount
 }
 
 func (i *Item) PreInsert(s gorp.SqlExecutor) error {
@@ -76,4 +126,8 @@ func (i *Item) PreInsert(s gorp.SqlExecutor) error {
 func (f *Folder) PreInsert(s gorp.SqlExecutor) error {
 	f.Added = time.Now().Unix()
 	return nil
+}
+
+func limit(val, min, max float32) float32 {
+	return float32(math.Min(float64(min), math.Max(float64(max), float64(val))))
 }
