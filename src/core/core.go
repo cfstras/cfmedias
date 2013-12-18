5f0ab5c300afca51e0abdf3ecd022a7fedf50766
Claus Strasburger <claus@strasburger.de>
2013-12-18 18:11:20 +0100

    Added basic notion of permission levels

--- src/core/core.go
+++ src/core/core.go
@@ -7,14 +7,33 @@ import (
 
 type ArgMap map[string][]string
 
+type AuthLevel uint
+
+const (
+	// Guests can only view public data
+	AuthGuest AuthLevel = iota
+
+	// Users can view and edit their own data
+	AuthUser
+
+	// Admins can edit other user data and most configuration
+	AuthAdmin
+
+	// Roots can edit all configuration, do database maintenance
+	// and stop the server
+	AuthRoot
+)
+
 type Command struct {
-	Verbs   []string
-	Help    string
-	Handler func(args ArgMap, w io.Writer) error
+	Verbs        []string
+	Help         string
+	MinAuthLevel AuthLevel
+	Handler      func(args ArgMap, w io.Writer) error
 }
 
 var (
 	ErrorCmdNotFound = errors.New("Command not found!")
+	ErrorNotAllowed  = errors.New("You are not allowed to do that!")
 )
 
 type Core interface {
@@ -25,5 +44,5 @@ type Core interface {
 	RegisterCommand(Command)
 	UnregisterCommand(Command)
 	CmdLine()
-	Cmd(cmd string, args ArgMap, w io.Writer) error
+	Cmd(cmd string, args ArgMap, w io.Writer, level AuthLevel) error
 }
