971ddca95a5135ea5432aacbc219bfbdd8ee8a8c
Claus Strasburger <claus@strasburger.de>
2014-05-27 22:03:54 +0200

    a lot of martini, deleting some code

--- src/web/webcmd.go
+++ src/web/webcmd.go
@@ -5,13 +5,11 @@ import (
 	"config"
 	"core"
 	"db"
-	"encoding/json"
 	"errrs"
 	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"html/template"
-	log "logger"
 	"mime"
 	"net/http"
 	_ "net/http/pprof"
@@ -33,17 +31,6 @@ type NetCmdLine struct {
 // context information for web templates
 type WebTemplate struct {
 	ApiPath string
-}
-
-func (n *NetCmdLine) getCmd(r *http.Request) (err error, cmd string, args core.ArgMap) {
-	path := r.URL.Path[1:]
-	parts := strings.Split(path, "/")
-	// ignore parts[2:]
-	if err := r.ParseForm(); err != nil {
-		return errrs.New("bad request: " + err.Error()), "", nil
-	}
-
-	return nil, parts[1], core.ArgMap(r.Form)
 }
 
 func (n *NetCmdLine) api(params martini.Params, r render.Render, req *http.Request) (
@@ -95,101 +82,6 @@ func (n *NetCmdLine) api(params martini.Params, r render.Render, req *http.Reque
 	}
 }
 
-func (n *NetCmdLine) printResult(w http.ResponseWriter, result core.Result) {
-	if result.Error != nil {
-		log.Log.Println("api error:", result.Status, result.Error)
-	}
-	if result.IsRaw {
-		fmt.Fprint(w, result.Status)
-		if result.Error != nil {
-			fmt.Fprintln(w, "", result.Error)
-		} else {
-			fmt.Fprintln(w)
-		}
-		if result.Result != nil {
-			if array, ok := result.Result.([]interface{}); ok {
-				for _, v := range array {
-					fmt.Fprintln(w, v)
-				}
-			} else {
-				fmt.Fprintln(w, result.Result)
-			}
-		}
-	} else {
-		if bytes, err := json.MarshalIndent(result, "", "  "); err != nil {
-			http.Error(w, err.Error(), 500)
-		} else {
-			w.Write(bytes)
-			fmt.Fprintln(w)
-		}
-	}
-}
-
-func (n *NetCmdLine) serveAsset(w http.ResponseWriter, r *http.Request) {
-	r.URL.Path = strings.TrimLeft(r.URL.Path, "/")
-	if r.URL.Path == "" {
-		r.URL.Path = "index.html"
-	}
-
-	cache := false
-	templateAble := false
-	for _, suffix := range allowedTemplates {
-		if strings.HasSuffix(r.URL.Path, suffix) {
-			cache = config.Current.CacheWebTemplates
-			templateAble = true
-			break
-		}
-	}
-	if templateAble {
-		var tmpl *template.Template
-		if cache {
-			tmpl = n.templates[r.URL.Path]
-		}
-		if tmpl == nil {
-			data, err := Asset(r.URL.Path)
-			if len(data) == 0 || err != nil {
-				//TODO handle 404's more nicely
-				w.WriteHeader(http.StatusNotFound)
-				fmt.Fprintln(w, err)
-				return
-			}
-			tmpl, err = template.New(r.URL.Path).Parse(string(data))
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Fprintln(w, err)
-				log.Log.Println(err)
-				return
-			}
-			n.templates[r.URL.Path] = tmpl
-		}
-		n.setMime(w, r)
-		err := tmpl.Execute(w, n.renderInfo)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintln(w, err)
-			log.Log.Println(err)
-		}
-	} else {
-		data, err := Asset(r.URL.Path)
-		if len(data) == 0 || err != nil {
-			//TODO handle 404's more nicely
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, err)
-			return
-		}
-		n.setMime(w, r)
-		_, err = w.Write(data)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprintln(w, err)
-		}
-	}
-}
-
-func (n *NetCmdLine) setMime(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", mime.TypeByExtension(r.URL.Path[strings.LastIndex(r.URL.Path, "."):]))
-}
-
 func (n *NetCmdLine) Start(core core.Core, db *db.DB) {
 	n.core = core
 	n.db = db
@@ -202,15 +94,22 @@ func (n *NetCmdLine) Start(core core.Core, db *db.DB) {
 	m := martini.Classic()
 	n.martini = m
 	m.Use(render.Renderer())
+
 	m.Group("/api", func(r martini.Router) {
 		r.Get("/:cmd", n.api)
 		r.Post("/:cmd", n.api)
 	})
+
+	m.Use(func(c martini.Context, r *http.Request) {
+		c.Map(r.URL.Path[1:])
+	})
 	m.Get("/", index_html)
-	m.Get("/js/**", n.serveAsset)
-	m.Get("/css/**", n.serveAsset)
-	m.Get("/fonts/**", n.serveAsset)
+	m.Get("/**", setMime, Asset)
 
 	os.Setenv("PORT", fmt.Sprint(config.Current.WebPort))
 	m.Run()
+}
+
+func setMime(w http.ResponseWriter, path string) {
+	w.Header().Set("Content-Type", mime.TypeByExtension(path[strings.LastIndex(path, "."):]))
 }
