53c9e56757add0f3f217c427e61f2c35b9f4d8c7
Claus Strasburger <claus@strasburger.de>
2015-01-04 21:31:41 +0100

    basic example

--- web/assets/js/application.js
+++ web/assets/js/application.js
@@ -2,3 +2,24 @@ window.App = Em.Application.create();
 
 App.Router.map(function() {
 });
+
+App.ApplicationView = Em.View.extend({
+    classNames: ['height'],
+});
+
+App.IndexRoute = Em.Route.extend({
+    model: function() {
+        var res = this.controllerFor('api').get('list', {
+            query: "artist LIKE 'dead%'"
+        });
+        return Em.ArrayController.create(res);
+    },
+});
+
+App.ApiController = Em.Controller.extend({
+    authToken: null,
+
+    get: function(url, options) {
+        return $.getJSON(apiurl+url, options);
+    },
+})
