81cadb75235227824a8869e087d2d9f92af5983a
Claus Strasburger <claus@strasburger.de>
2015-01-04 19:42:49 +0100

    first test of list

--- web/assets/js/application.js
+++ web/assets/js/application.js
@@ -1,4 +1,26 @@
 window.App = Em.Application.create();
 
-App.Router.map(function() {
+App.Router.map(function() {});
+
+App.ListView = Ember.ListView.extend({
+    height: 500,
+    rowHeight: 35,
+    //elementWidth: 80,
+    //width: 500,
+    itemViewClass: Ember.ListItemView.extend({
+        templateName: "index_row"
+    })
+});
+
+App.IndexRoute = Ember.Route.extend({
+    model: function() {
+        var items = [];
+        for (var i = 0; i < 100; i++) {
+            items.push({
+                col_1: "Item",
+                col_2: i,
+            });
+        }
+        return items;
+    }
 });
